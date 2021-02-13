65a7157545b5cffbdcf5c1caf3eca4c81a8a154c
perought <52033538+perought@users.noreply.github.com>
2021-02-14 00:50:43 +0300

    add ssl for database

--- index.js
+++ index.js
@@ -44,6 +44,9 @@ const { Pool, Client } = require('pg')
 // const pool = new Pool()
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 client.connect()
 
